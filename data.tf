3693a7c0ec039fc7b741b5c6c0c32f2793edd4bd
Brett Wright <brett.wright@bestseller.com>
2020-12-09 13:36:40 +0100

    lets drink beer

--- data.tf
+++
@@ -1,4 +0,0 @@
-data "google_compute_zones" "available" {
-    project = var.project
-    region = var.region
-}
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_compute_instance" "main" {
     name = var.name
     project = var.project
     machine_type = "n1-standard-1"
-    zone         = data.google_compute_zones.available[count.index]
+    zone         = var.zone
 
     boot_disk {
     initialize_params {

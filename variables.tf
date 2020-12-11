cd2da3d7ec9b693a79b6996ed65a986b0f39223e
Brett Wright <brett.wright@bestseller.com>
2020-12-11 12:31:47 +0100

    playing with network name

--- variables.tf
+++ variables.tf
@@ -29,7 +29,9 @@ variable "os_version" {
 }
 
 locals {
-  network    = "projects/network-host-project-5361/global/networks/network-host-project-${data.google_project.project.labels["trust_zone"]}-trusted-zone-shared-vpc"
+  network    = "projects/network-host-project-5361/regions/europe-west4/subnetworks/${data.google_project.project.name}-default"
+  #     /network-host-project-${data.google_project.project.labels["trust_zone"]}-trusted-zone-shared-vpc"
+  # projects/network-host-project-5361/regions/europe-west4/subnetworks/jazz-pri-dev-default
   # db_vm_size = "db-custom-${var.cpu_cores}-${var.ram_gb * 1024}"
   env        = var.env == "" ? data.google_project.project.labels["env"] : var.env
 }
